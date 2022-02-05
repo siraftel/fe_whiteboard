5c330939eec95492a2e9e3a26c8340372eba9e64
akbaradam24 <adam.akhbar24@gmail.com>
2022-02-05 20:51:14 +0900

    sidebar new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-// import Sidebar from "../src/Components/ShareComponent/Sidebar/Sidebar";
-import Homepage from "../src/Pages/Homepage";
+import Sidebar from "../src/Components/ShareComponent/Sidebar/Sidebar";
+// import Homepage from "../src/Pages/Homepage";
 function App() {
   return (
     <div>
-      <Homepage />
-      {/* <Sidebar /> */}
+      {/* <Homepage /> */}
+      <Sidebar />
     </div>
   );
 }
