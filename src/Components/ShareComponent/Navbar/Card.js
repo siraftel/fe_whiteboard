b347ea4ad1d7cdb097961994e02f2d60d4804776
akbaradam24 <adam.akhbar24@gmail.com>
2022-02-09 21:49:18 +0900

    Feature Home

--- src/Components/ShareComponent/Navbar/Card.js
+++ src/Components/ShareComponent/Navbar/Card.js
@@ -12,8 +12,12 @@ export default function Card() {
         <h2 className={style.projectTitle}>Idev Internal System</h2>
         <p className={style.teamProject}>on Idev Project</p>
         <h2 className={style.projectShortcut}>QUICK LINKS</h2>
-        <p className={style.teamTask}>My open tasks</p>
-        <p className={style.taskDone}>Done</p>
+        <a href="/" className={style.teamTask}>
+          My open tasks
+        </a>
+        <a href="/" className={style.taskDone}>
+          Done
+        </a>
       </div>
     </section>
   );
