d618350cce30867427f481ed4330bd934a076ab8
akbaradam24 <adam.akhbar24@gmail.com>
2022-02-09 10:30:46 +0900

    Card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import Sidebar from "../src/Components/ShareComponent/Sidebar/Sidebar";
+// import Sidebar from "../src/Components/ShareComponent/Sidebar/Sidebar";
 // import Homepage from "../src/Pages/Homepage";
 // import Login from "../src/Pages/Login";
+import Card from "../src/Components/ShareComponent/Navbar/Card";
 function App() {
   return (
     <div>
       {/* <Homepage /> */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Card />
       {/* <Login /> */}
     </div>
   );
