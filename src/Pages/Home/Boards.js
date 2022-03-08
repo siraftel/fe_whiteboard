749cf2a2786a95faeb971240c96c2ba4b5cdd09e
akbaradam24 <adam.akhbar24@gmail.com>
2022-03-08 22:26:02 +0800

    checkpoint minor add

--- src/Pages/Home/Boards.js
+++ src/Pages/Home/Boards.js
@@ -3,7 +3,10 @@ import NavbarIsLogin from "../../Components/ShareComponent/Navbar/NavbarIsLogin"
 import SidebarStatic from "../../Components/ShareComponent/Sidebar/SidebarStatic";
 import style from "../../Styling/Pages/Home/Boards.module.css";
 import Card from "../../Components/ShareComponent/Navbar/Card";
+import { useSelector } from "react-redux";
 export default function Boards() {
+  const getTeam = useSelector((state) => state.teamReducer.teams);
+  console.log(getTeam);
   return (
     <>
       <NavbarIsLogin />
@@ -13,37 +16,14 @@ export default function Boards() {
         </div>
         <div className={style.Boards}>
           <h1>Boards</h1>
-          <div className={style.Board}>
-            <h5>Idev Project</h5>
-            <div className={style.cards}>
-              <Card />
+          {getTeam.map((e, _id) => (
+            <div key={_id} className={style.Board}>
+              <h5>{e.teamName}</h5>
+              <div className={style.cards}>
+                <Card />
+              </div>
             </div>
-          </div>
-          <div className={style.Board}>
-            <h5>e-Project</h5>
-            <div className={style.cards}>
-              <Card />
-              <Card />
-            </div>
-          </div>
-          <div className={style.Board}>
-            <h5>Everyone Education Center</h5>
-            <div className={style.cards}>
-              <Card />
-            </div>
-          </div>
-          <div className={style.Board}>
-            <h5>One by Meja Putih</h5>
-            <div className={style.cards}>
-              <Card />
-            </div>
-          </div>
-          <div className={style.Board}>
-            <h5>One by Meja Putih</h5>
-            <div className={style.cards}>
-              <Card />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
